d841f9649f5e0fcc7f108c1199c3eedbb32562a3
Jan Philipp Harries <jpdus@users.noreply.github.com>
2023-10-27 18:01:18 +0200

    bump version fix tests

--- openai_request_runner/__init__.py
+++ openai_request_runner/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.4"
-from .openaischema import OpenAISchema
-from .request_runner import process_api_requests_from_list
-from .utils import append_to_jsonl
+__version__ = "0.0.6"
+from .openaischema import OpenAISchema  # noqa: F401
+from .request_runner import process_api_requests_from_list  # noqa: F401
+from .utils import append_to_jsonl  # noqa: F401
